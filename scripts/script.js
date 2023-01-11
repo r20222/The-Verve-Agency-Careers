368d22cbe29b3cc11db5a5e55213c3423968b7c8
r20222 <101579892+r20222@users.noreply.github.com>
2023-01-11 17:55:57 +0100

    JS en design-history

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+const cursor = document.querySelector('.cursor');
+const designHistory = document.querySelector('.design-history');
+
+designHistory.addEventListener('mousemove', e=> {
+console.log(e);
+// cursor.setAttribute
+})
+
+// https://www.youtube.com/watch?v=rfpRZ2t_BrQ
+// 10:14
