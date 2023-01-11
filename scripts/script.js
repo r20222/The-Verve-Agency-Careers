ebb632f6bcab8b482cf476bc3b68f44aedaa580d
r20222 <101579892+r20222@users.noreply.github.com>
2023-01-11 20:20:23 +0100

    vette cursor

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const designHistory = document.querySelector('.design-history');
 
 designHistory.addEventListener('mousemove', e=> {
 console.log(e);
-// cursor.setAttribute
+cursor.setAttribute('style', 'top: '+(e.pageY - 30.5)+"px; left: "+(e.pageX - 29)+"px;")
 })
 
 // https://www.youtube.com/watch?v=rfpRZ2t_BrQ
