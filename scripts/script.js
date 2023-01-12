c96c602c4d17e604e55e6288e7442ced629abc99
r20222 <101579892+r20222@users.noreply.github.com>
2023-01-12 14:57:02 +0100

    index ipv careers

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// Design history section
+
 const cursor = document.querySelector('.cursor');
 const designHistory = document.querySelector('.design-history');
 
@@ -8,3 +10,20 @@ cursor.setAttribute('style', 'top: '+(e.pageY - 30.5)+"px; left: "+(e.pageX - 29
 
 // https://www.youtube.com/watch?v=rfpRZ2t_BrQ
 // 10:14
+
+
+// skateboard dog section
+
+document.querySelector('body').addEventListener('mousemove', eyeball);
+
+
+function eyeball(){
+  var eye = document.querySelector('.eye');
+  
+    let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
+    let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
+    let radian = Math.atan2(event.pageX - x, event.pageY - y);
+    let rot = (radian * (180 / Math.PI) * -1) - 270;
+    eye.style.transform = "rotate("+ rot +"deg)";
+}
+
