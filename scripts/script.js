f2706df3b005ef866738431dad9dc1c8fb825306
r20222 <101579892+r20222@users.noreply.github.com>
2023-01-18 11:23:06 +0100

    scroll animation

--- scripts/script.js
+++ scripts/script.js
@@ -129,3 +129,22 @@ function roll4(){
 // function roll() {
 //   app.classList.toggle('roll-board');
 // }
+
+
+
+// Scroll animation
+// bron: https://www.youtube.com/watch?v=T33NN_pPeNI
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) =>{
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    } 
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
