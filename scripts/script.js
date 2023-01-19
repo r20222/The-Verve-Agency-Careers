0e0a0f6f400f1e385d9b80474ed243451767114a
r20222 <101579892+r20222@users.noreply.github.com>
2023-01-19 17:26:17 +0100

    carrousel met foto's

--- scripts/script.js
+++ scripts/script.js
@@ -148,3 +148,27 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+// image carrousel
+// bron: https://www.youtube.com/watch?v=9HcxHDS2w1s
+
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach( button => {
+  button.addEventListener("click", () =>{
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button.closest("[data-carousel]")
+    .querySelector("[data-slides]")
+
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+
+    if (newIndex < 1) newIndex = slides.children.length -1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+} )
