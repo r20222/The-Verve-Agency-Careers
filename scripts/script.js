34227586a0e2c7c8cedc955fdc94d6543d959403
r20222 <101579892+r20222@users.noreply.github.com>
2023-01-20 09:15:26 +0100

    waarom doet mn carousel het niet?

--- scripts/script.js
+++ scripts/script.js
@@ -153,22 +153,22 @@ hiddenElements.forEach((el) => observer.observe(el));
 // image carrousel
 // bron: https://www.youtube.com/watch?v=9HcxHDS2w1s
 
-const buttons = document.querySelectorAll("[data-carousel-button]")
+// const buttons = document.querySelectorAll("[data-carousel-button]")
 
-buttons.forEach( button => {
-  button.addEventListener("click", () =>{
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1
-    const slides = button.closest("[data-carousel]")
-    .querySelector("[data-slides]")
+// buttons.forEach( button => {
+//   button.addEventListener("click", () =>{
+//     const offset = button.dataset.carouselButton === "next" ? 1 : -1
+//     const slides = button.closest("[data-carousel]")
+//     .querySelector("[data-slides]")
 
 
-    const activeSlide = slides.querySelector("[data-active]")
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+//     const activeSlide = slides.querySelector("[data-active]")
+//     let newIndex = [...slides.children].indexOf(activeSlide) + offset
 
-    if (newIndex < 1) newIndex = slides.children.length -1
-    if (newIndex >= slides.children.length) newIndex = 0
+//     if (newIndex < 1) newIndex = slides.children.length -1
+//     if (newIndex >= slides.children.length) newIndex = 0
 
-    slides.children[newIndex].dataset.active = true
-    delete activeSlide.dataset.active
-  })
-} )
+//     slides.children[newIndex].dataset.active = true
+//     delete activeSlide.dataset.active
+//   })
+// } )
